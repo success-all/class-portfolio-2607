fc838c83e8b01767f70bafd24a1c6372427502db
frontierall <frontierall@gmail.com>
2026-07-07 13:25:13 +0900

    Set shared ingest token to match Apps Script script property

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.TRACKER_CONFIG = {
   APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbz-uySEPLvuGmoeeiszFC5HR-wNvTtFiwtFOiSwkq1I5n7X-MoY3mqeW1WaI3j1PZaupw/exec',
-  INGEST_TOKEN: 'REPLACE_WITH_SHARED_TOKEN'
+  INGEST_TOKEN: 'd0d2310d42ac635f3e6694c8d9d9d833'
 };
