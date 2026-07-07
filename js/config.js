f12f52548148a4b34ca334ddfb5a318e250472d6
frontierall <frontierall@gmail.com>
2026-07-07 13:22:58 +0900

    Update Apps Script web app URL to the new deployment
    Previous URL's script had a syntax error after copy-pasting the
    updated Code.gs, so a fresh deployment was created with a new URL.

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.TRACKER_CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwyxEu9DucHyIvTDnx2WfMk1haseLmdNrwUaekZw7BQHAnD1w1oF9rap6WOdvgFXwdFeA/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbz-uySEPLvuGmoeeiszFC5HR-wNvTtFiwtFOiSwkq1I5n7X-MoY3mqeW1WaI3j1PZaupw/exec',
   INGEST_TOKEN: 'REPLACE_WITH_SHARED_TOKEN'
 };
